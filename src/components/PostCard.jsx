fc02cca83579e79e7a3217e63348d33b7dc2768f
Abdul-Rafey Shaikh <as05324@surrey.ac.uk>
2026-08-31 09:49:53 +0100

    KAN-7: add PostCard component with number formatting and thumbnail fallback

---
+++ src/components/PostCard.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { FiArrowUp, FiArrowDown, FiMessageSquare, FiImage } from "react-icons/fi";
+import { formatUps } from "../utils/formatNumbers";
+function PostCard({id, title, author, subreddit, createdAt, ups, numComments, thumbnail, permalink}) {
+    
+    return (
+        
+        <article>
+            <div>
+            <span className="vote-btn-up">
+                <FiArrowUp aria-hidden="true"></FiArrowUp>
+            </span>
+            <p>
+            {formatUps(ups)}
+            </p>
+            <span className="vote-btn-down">
+                <FiArrowDown aria-hidden="true"/>
+            </span>
+            </div>
+               
+            <div>
+                {thumbnail ? <img src={thumbnail} alt={title}/> : <FiImage aria-hidden="true"></FiImage>}  
+            </div>
+            <div>
+                <p>{`r/${subreddit} · u/${author} · ${createdAt}`}</p>
+                <h3>{title}</h3>
+                <p><span><FiMessageSquare aria-hidden="true"></FiMessageSquare></span> {numComments === 1 ? `${formatUps(numComments)} comment`: `${formatUps(numComments)} comments` }</p>
+            </div>
+        </article>
+    );
+}
+
+
+export default PostCard;
