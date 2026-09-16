2d7e4bd87bd394c63abb419220e31cc1d8544a1b
Abdul-Rafey Shaikh <abdioshaikh61@gmail.com>
2026-09-16 02:04:32 +0100

    KAN-7: complete Home.jsx with dispatch, useSelector, and status-based

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,32 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { fetchPosts } from '../features/posts/postsSlice.js';
+import PostCard from '../components/PostCard.jsx';
+
 
 function Home() {
     const dispatch = useDispatch();
+    const items = useSelector(state => state.posts.items);
+    const ids = useSelector(state => state.posts.ids);
+    const status = useSelector(state => state.posts.status);
+   
     useEffect(() =>
  {
     dispatch(fetchPosts({subreddit: 'popular', sort: 'hot'}));
 }, [])
-
+    let content;
+        if(status === 'loading' || status === 'idle') {
+            content = 'Currently waiting to get posts';
+        }
+        else if(status === 'failed') {
+            content = 'failed to get posts';
+        }
+        else {
+            content = ids.map(id => <PostCard key={id} {...items[id]}/>);
+        }
 return  (
     <>
+    {content}
     </>
 )
 }
